5f5c5c31168a1a992dbb6426a7034403f0a65433
Aditya Gupta <iamadityagupta888@gmail.com>
2026-03-25 08:33:13 +0530

    Update Tinker3.py

--- Tinker3.py
+++ Tinker3.py
@@ -17,3 +17,23 @@ elif c == "/":
     print("The quotient of", a, "and", b, "is", a / b)
 else:
     print("Invalid operator")
+
+#====if want more efficient or in short format====
+
+x = input("Enter your name :")
+print("Welcome to your calculator ,", x)
+a = float(input("Enter the first number :"))
+b = float(input("Enter the second number :"))
+print("Enter the operator (+ , - , * , /) : ")
+c = input()
+if c == "+":
+    print("The sum of", a, "and", b, "is", a + b)
+elif c == "-":
+    print("The difference of", a, "and", b, "is", a - b)
+elif c == "*":
+    print("The product of", a, "and", b, "is", a * b)
+elif c == "/":
+    print("The quotient of", a, "and", b, "is", a / b)
+else:
+    print("Invalid operator")
+
